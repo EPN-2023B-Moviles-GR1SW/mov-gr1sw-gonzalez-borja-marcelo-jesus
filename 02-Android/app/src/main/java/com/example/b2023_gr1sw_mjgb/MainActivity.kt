41d99db7a4a8f31179b3fb45675a5b67f1cf24d3
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2023-12-06 14:12:51 -0500

    actividades

--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_mjgb/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_mjgb/MainActivity.kt
@@ -1,10 +1,22 @@
 package com.example.b2023_gr1sw_mjgb
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_name)
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida.setOnClickListener {
+            irActividad(ACicloVida::class.java)
+        }
+    }
+    fun irActividad(clase: Class<*>){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
