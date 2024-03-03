e337aabafe3c1fff164e5b603248d3effe308967
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2024-03-02 22:29:30 -0500

    initial fragments

--- 10-Proyecto/app/build.gradle.kts
+++ 10-Proyecto/app/build.gradle.kts
@@ -26,6 +26,12 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        viewBinding = true
+        dataBinding = true
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
--- 10-Proyecto/app/src/main/java/com/example/proyecto/MainActivity.kt
+++ 10-Proyecto/app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.proyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.proyecto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(MiListaFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.milista -> replaceFragment(MiListaFragment())
+                R.id.animes -> replaceFragment(AnimeFragment())
+                R.id.explorar -> replaceFragment(ExplorarFragment())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout_main,fragment)
+        fragmentTransaction.commit()
     }
 }
